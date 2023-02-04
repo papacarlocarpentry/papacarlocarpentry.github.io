546c55da79676a73142219e12176c45f596c4e5d
Kirill kine Nevzorov <akinevz@outlook.com>
2023-02-04 18:39:25 +0000

    made header collapsible

--- src/main/scala/dev/papacarlo/Library.scala
+++ src/main/scala/dev/papacarlo/Library.scala
@@ -1,11 +1,25 @@
 package dev.papacarlo
 import org.scalajs.dom._
-import org.scalajs.dom.document._
+import org.scalajs.dom.document
 
 object Library {
 
+  def header() = document.querySelector(".banner")
+
   def main(args: Array[String]): Unit = startup()
 
-  def startup(): Unit = {}
+  def startup(): Unit = {
+    registerScrollListener() { scroll =>
+      if (pageScroll() > 0) collapseHeader()
+      else expandHeader()
+    }
+  }
 
+  def registerScrollListener()(func: Event => Unit): Unit =
+    document.addEventListener("scroll", func)
+
+  def pageScroll() = document.documentElement.scrollTop
+
+  def collapseHeader() = header().classList.add("collapsed")
+  def expandHeader() = header().classList.remove("collapsed")
 }
